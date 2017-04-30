a959d6998f60ecdf7fbfa29583d8703b7c9f7402
chenyingjing <dcba63@gmail.com>
2017-04-30 18:01:39 +0800

    Tutorial 3: First Triangle on iOS

---
+++ iOS/SBS03/SBS03/SBS03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SBS03
+//
+//  Created by aa64mac on 30/04/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/SBS03/SBS03/SBS03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SBS03
+//
+//  Created by aa64mac on 30/04/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
