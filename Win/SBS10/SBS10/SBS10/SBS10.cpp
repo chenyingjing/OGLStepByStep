c06af5b7c36387e4d30042693ecb2dda0e63967e
chenyingjing <dcba63@gmail.com>
2017-05-06 15:24:25 +0800

    Change the init height from 100 to 20.

--- Win/SBS10/SBS10/SBS10/SBS10.cpp
+++ Win/SBS10/SBS10/SBS10/SBS10.cpp
@@ -176,7 +176,7 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1024, 768);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowPosition(100, 20);
 	glutCreateWindow("Tutorial 10");
 
 	InitializeGlutCallbacks();
