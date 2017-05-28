fbce136f7e08f28de51d2ed34b22c432e4f768ec
chenyingjing <dcba63@gmail.com>
2017-05-28 16:50:45 +0800

    cull face

--- Mac/SBS24/SBS24/SBS24/SBS24.cpp
+++ Mac/SBS24/SBS24/SBS24/SBS24.cpp
@@ -444,6 +444,7 @@ int main(void)
 	glDepthFunc(GL_LESS);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_CULL_FACE);
 
 	LoadWoodenCrateAsset();
     LoadGroundAsset();
