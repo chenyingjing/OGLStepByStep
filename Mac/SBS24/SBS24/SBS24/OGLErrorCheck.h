8a313186a9838f8a35f4ee49f2504583b1a5f53d
chenyingjing <dcba63@gmail.com>
2017-06-07 21:41:04 +0800

    Add error check.

---
+++ Mac/SBS24/SBS24/SBS24/OGLErrorCheck.h
@@ -0,0 +1,17 @@
+//
+//  OGLErrorCheck.hpp
+//  SBS24
+//
+//  Created by chenyingjing on 07/06/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#ifndef OGLErrorCheck_hpp
+#define OGLErrorCheck_hpp
+
+#include "glew.h"
+#include <stdio.h>
+
+void checkError();
+
+#endif /* OGLErrorCheck_hpp */
